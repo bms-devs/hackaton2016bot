09cf685bfda4695a477f18e75421d312e010e9e9
Bolek <boleslaw.tekielski@bms.com.pl>
2016-12-17 13:32:36 +0100

    Added config np package

--- bin/wcbot.js
+++ bin/wcbot.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var WcBot = require('../lib/wcbot');
+var config = require('config');
 
 var wcbot = new WcBot({
     token: process.env.BOT_API_KEY,
     httpOptions: {
-      host: 'www.wp.pl',
-      path: '/'
+      host: config.get('host'),
+      path: config.get.path
     }
 });
 
