7fb69389eec895d9b60c0a457e2e04c38bfff245
pushpull <pushpull@localhost>
2016-12-17 11:15:10 +0100

    First attempt to create a working bot

---
+++ bin/bot.js
@@ -0,0 +1,18 @@
+# bin/bot.js
+
+'use strict';
+
+var Bot = require('slackbots');
+
+var settings = {
+  token: 'xoxb-117440560689-VvqPmvToCVQzF8yAFodajvw9',
+  name: 'WC Bot'
+};
+
+var bot = new Bot(settings);
+
+bot.on('start', function() {
+  bot.postMessageToChannel('wc-test', 'siema heniu!');
+});
+
+bot.run();
