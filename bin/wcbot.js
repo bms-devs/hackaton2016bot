ee38337aa360494eea2a114baaa185ef78696166
pushpull <pushpull@localhost>
2016-12-17 14:01:09 +0100

    Working https connection with heroku app

--- bin/wcbot.js
+++ bin/wcbot.js
@@ -5,10 +5,10 @@ var config = require('config');
 
 var wcbot = new WcBot({
     token: process.env.BOT_API_KEY,
-    httpOptions: {
-      host: config.get.host,
-      path: config.get.path,
-      token: process.env.API_TOKEN || config.get.token
+    httpsOptions: {
+      token: process.env.API_TOKEN || config.get.token,
+      host: config.get("host"),
+      path: config.get("path")
     }
 });
 
